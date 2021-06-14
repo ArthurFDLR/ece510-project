996fd23656e29e13cd1a00403ccd3536cee62265
ArthurFDLR <arthfind@gmail.com>
2021-06-14 15:32:50 -0500

    🎉WiFi scan successful

--- M5StickCPlus/src/main.cpp
+++ M5StickCPlus/src/main.cpp
@@ -1,31 +1,19 @@
 #include "M5StickCPlus.h"
-#include "WiFi.h"
+#include <WiFi.h>
 
-
-char WIFI_SSID[] =  "TP-Link_AF58";
-const char* WIFI_PASS = "74179510";
+char* ssid       = "FakeWifiHotspot";
+char* password   = "password";
 
 void setup() {
-  M5.begin();
-  Serial.begin(115200);
-  Serial.printf("Connecting to %s ", WIFI_SSID);
-  WiFi.begin(WIFI_SSID, WIFI_PASS);
-  Serial.printf("bonjour");
-  while (WiFi.status() != WL_CONNECTED) {
-      delay(500);
-      Serial.print(".");
-  }
-  Serial.println("CONNECTED");
+    M5.begin(true, true, true);
+    Serial.printf("Connecting to %s ", ssid);
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println(" CONNECTED");
 }
 
 void loop() {
-    Serial.println("Loop");
-    if (WiFi.status() == WL_CONNECTED) {
-        Serial.println("WiFi connected");
-        Serial.println(WiFi.localIP().toString());
-    } else {
-        Serial.println("WiFi not connected");
-    }
-    
-
 }
